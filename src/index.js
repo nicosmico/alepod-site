057c45798529abad1e92ddd5ab22815c970d5f51
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-07-03 23:10:41 -0400

    feat(*): add navbar active and fix other styles

--- src/index.js
+++ src/index.js
@@ -3,20 +3,35 @@ import './assets/styles/styles.css';
 function toggleNavbar() {
   const navbarList = document.querySelector('.navbar__list');
   const navbarToggle = document.querySelector('.navbar__toggle');
+  if (navbarList?.getAttribute('data-visible') === 'true') {
+    navbarList?.setAttribute('data-visible', false);
+    navbarToggle?.setAttribute('aria-expanded', false);
+  } else {
+    navbarList?.setAttribute('data-visible', true);
+    navbarToggle?.setAttribute('aria-expanded', true);
+  }
+}
 
-  navbarToggle?.addEventListener('click', () => {
-    if (navbarList?.getAttribute('data-visible') === 'true') {
-      navbarList?.setAttribute('data-visible', false);
-      navbarToggle?.setAttribute('aria-expanded', false);
+function styleActiveLink() {
+  const navbarLinks = document.querySelectorAll('.navbar__link');
+  navbarLinks.forEach((link) => {
+    if (link.href === window.location.href) {
+      link.classList.add('active');
     } else {
-      navbarList?.setAttribute('data-visible', true);
-      navbarToggle?.setAttribute('aria-expanded', true);
+      link.classList.remove('active');
     }
   });
 }
 
 function listeners() {
-  toggleNavbar();
+  const navbarToggle = document.querySelector('.navbar__toggle');
+  navbarToggle?.addEventListener('click', () => {
+    toggleNavbar();
+  });
+
+  window.addEventListener('hashchange', () => {
+    styleActiveLink();
+  });
 }
 
 listeners();
