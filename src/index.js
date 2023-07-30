f16b86194315200f3eee46c3477c8a283e2ea057
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-07-29 20:19:01 -0400

    feat(comments): Add star icons to reviews

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './assets/styles/styles.css';
 import Navbar from './app/components/navbar';
 import GooglePlaces from './app/components/services/google-places';
 
+// Images
+import star from './assets/icons/star.svg';
+import starFilled from './assets/icons/star-filled.svg';
+
 function main() {
   const navbar = new Navbar();
   navbar.init();
@@ -31,19 +35,27 @@ function main() {
   });
 
   // Reviews
-  GooglePlaces.getReviews().then((data) => {
-    console.log(data);
-    data.forEach((review) => {
-      const reviews = document.querySelector('.reviews');
+  const getStarRanking = (ranking) => {
+    const starImg = `<img src="${star}" class="comment__star" alt="Estrella">`;
+    const filledStarImg = `<img src="${starFilled}" class="comment__star" alt="Estrella completada">`;
+    return filledStarImg.repeat(ranking) + starImg.repeat(5 - ranking);
+  };
 
-      if (review.review.rating > 4 && review.review.content) {
+  GooglePlaces.getReviews().then((data) => {
+    const reviews = document.querySelector('.reviews');
+    data.forEach((review) => {
+      // eslint-disable-next-line no-param-reassign
+      review.review.rating = 4;
+      if (review.review.rating >= 4 && review.review.content) {
         reviews.innerHTML += `<div class="comment box-shadow-2 spaced-y-05">
           <div class="flex-align-center gap-05">
             <img class="comment__avatar" src="${review.user.picture}" alt="Foto de perfil de cliente.">
             <div>
               <p class="txt-shadow-1 f-weight-500">${review.user.name}</p>
-              <p class="ft-label">${review.review.since}</p>
-              <p class="ft-label">${review.review.rating}</p>
+              <div class="flex-align-center gap-05">
+                <p class="ft-label">${review.review.since}</p>
+                <p class="ft-label flex">${getStarRanking(review.review.rating)}</p>
+              </div>
             </div>
           </div>
           <p>“${review.review.content}”</p>
