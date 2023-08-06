6cfa6e782830b4e64d996160ff98c6ab9be6a398
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-08-06 00:09:08 -0400

    feat(navbar): Add title

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -5,29 +5,31 @@ import logoAlepodSquare from '../../../assets/images/logo-alepod-square.svg';
 export default class Navbar extends BaseCustomElement {
   toggleButton = null;
   menuList = null;
+  navbarTitle = null;
 
   render() {
     return `
-      <button class="navbar__toggle" aria-expanded="false"><span class="sr-only">Menu</span></button>
-      <img class="navbar__logo" src="${logoAlepodSquare}" alt="Logo cuadrado de Alepod.">
+    <p class="navbar__title txt-primary">ALEPOD</p>
+    <img class="navbar__logo" src="${logoAlepodSquare}" alt="Logo cuadrado de Alepod.">
+    <button class="navbar__toggle" aria-expanded="false"><span class="sr-only">Menu</span></button>
 
-      <ul class="navbar__list bg-primary txt-on-primary flex-column gap-2" data-visible="false">
-        <li><a class="navbar__link uppercase ft-headline active" href="#home">
-          <span>01</span>Agendar hora
-        </a></li>
-        <li><a class="navbar__link uppercase ft-headline" href="#locations">
-          <span>02</span>Localidades
-        </a></li>
-        <li><a class="navbar__link uppercase ft-headline" href="#questions">
-          <span>03</span>Preguntas
-        </a></li>
-        <li><a class="navbar__link uppercase ft-headline" href="#comments">
-          <span>04</span>Comentarios
-        </a></li>
-        <li><a class="navbar__link uppercase ft-headline" href="#about-me">
-          <span>05</span>Sobre mi
-        </a></li>
-      </ul>
+    <ul class="navbar__list bg-primary txt-on-primary flex-column gap-2" data-visible="false">
+      <li><a class="navbar__link uppercase ft-headline active" href="#home">
+        <span>01</span>Agendar hora
+      </a></li>
+      <li><a class="navbar__link uppercase ft-headline" href="#locations">
+        <span>02</span>Localidades
+      </a></li>
+      <li><a class="navbar__link uppercase ft-headline" href="#questions">
+        <span>03</span>Preguntas
+      </a></li>
+      <li><a class="navbar__link uppercase ft-headline" href="#comments">
+        <span>04</span>Comentarios
+      </a></li>
+      <li><a class="navbar__link uppercase ft-headline" href="#about-me">
+        <span>05</span>Sobre mi
+      </a></li>
+    </ul>
   `;
   }
 
@@ -35,6 +37,7 @@ export default class Navbar extends BaseCustomElement {
     this.toggleButton = document.querySelector('.navbar__toggle');
     this.menuList = document.querySelector('.navbar__list');
     this.menuLinks = document.querySelectorAll('.navbar__link');
+    this.navbarTitle = document.querySelector('.navbar__title');
     this.updateActiveLink();
 
     this.toggleButton.addEventListener('click', () => {
@@ -59,11 +62,15 @@ export default class Navbar extends BaseCustomElement {
   openMenu() {
     this.menuList.setAttribute('data-visible', true);
     this.toggleButton.setAttribute('aria-expanded', true);
+    this.navbarTitle.classList.remove('txt-primary');
+    this.navbarTitle.classList.add('txt-on-primary');
   }
 
   closeMenu() {
     this.menuList.setAttribute('data-visible', false);
     this.toggleButton.setAttribute('aria-expanded', false);
+    this.navbarTitle.classList.add('txt-primary');
+    this.navbarTitle.classList.remove('txt-on-primary');
   }
 
   updateActiveLink() {
