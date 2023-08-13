14ac769f00165c911e7418b2b52b47287417203e
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-08-13 19:21:04 -0400

    refactor(homepage.cy): Add context

---
+++ cypress/e2e/01_homepage.cy.js
@@ -0,0 +1,33 @@
+import contactData from '../fixtures/contact-data.json';
+
+describe('01 - Homepage', () => {
+  context('Contact information', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/');
+      cy.viewport('iphone-x');
+    });
+
+    it('user can view phone number', () => {
+      cy.getBySel('header-phone').contains(contactData.formatedPhone);
+    });
+
+    it('user can click to Whatsapp button', () => {
+      cy.getBySel('contact-whatsapp')
+        .should('be.visible')
+        .should('not.have.attr', 'href', '#undefined');
+
+      cy.getBySel('contact-whatsapp').then((element) => {
+        const href = element.attr('href');
+        cy.request(href)
+          .should('have.property', 'status', 200);
+      });
+    });
+
+    it('user can click to call button', () => {
+      cy.getBySel('contact-call')
+        .should('be.visible')
+        .should('not.have.attr', 'href', '#undefined')
+        .should('have.attr', 'href', `tel:${contactData.phone}`);
+    });
+  });
+});
