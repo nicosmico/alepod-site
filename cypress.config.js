84658c541f638c3cb6c977687efce3d000179443
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2024-03-24 20:42:46 -0300

    fix(cypress.config): Add setupNodeEvents again

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here, don't delete this
+      console.log({ on, config });
+    },
+  },
   video: false,
   retries: {
     runMode: 3,
